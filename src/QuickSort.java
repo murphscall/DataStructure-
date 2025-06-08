b4aa89c1d8ea2f53f0e40d21b439da35b06024a4
kimjinhoo <wlsgnwkd22@naver.com>
2025-06-08 15:22:20 +0900

    퀵 정렬 구현

---
+++ src/QuickSort.java
@@ -0,0 +1,71 @@
+import java.time.chrono.MinguoChronology;
+
+public class QuickSort {
+
+	public static void quickSort(int[] arr , int left , int right){
+		if(left > right ) return;
+
+		int pivot = arr[left];
+		int start = left + 1;
+		int end = right;
+
+
+		while(start < end){
+
+			while(start <= end && arr[start] < pivot) start ++;
+
+			while(start <= end && arr[end] > pivot ) end--;
+
+
+			if(start < end) {
+				int temp = arr[start];
+				arr[start] = arr[end];
+				arr[end] = temp;
+			}
+
+		}
+
+		int temp = arr[left];
+		arr[left] = arr[end];
+		arr[end] = temp;
+
+		quickSort(arr , left , end -1);
+		quickSort(arr , end + 1, right);
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {5,3,4,7,8};
+
+
+		int left = 0;  // 가장 왼쪽
+		int right = arr.length -1; // 가장 오른쪽
+
+		int pivot = arr[left]; // 기준 값
+		int i = left + 1; // 기준값과 처음으로 비교 시작할 값
+		int j = right; //  마지막 가장 오른쪽 값
+
+		while ( i <=j ){
+			// i 가 마지막 인덱스 보다 작고 arr[i] 의 값이 기준값 보다 작으면 i ++-+
+			while(i <= right && arr[i] <= pivot) i++;
+
+			while (j > left && arr[j] >= pivot) j--;
+
+
+			if (i < j ){
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		if (left != j) {
+			int temp = arr[left];
+			arr[left] = arr[j];
+			arr[j] = temp;
+		}
+
+
+
+	}
+
+}
---
+++ src/SelectSort.java
@@ -0,0 +1,24 @@
+public class SelectSort {
+	public static void main(String[] args) {
+
+		int[] array = {5,3,4,1,2};
+
+		for(int i=0; i < array.length -1 ; i++){
+			int minIndex = i;
+
+			for(int j = i + 1; j < array.length; j++){
+				if(array[j] < array[minIndex]){
+					minIndex = j;
+				}
+			}
+			int temp = array[i];
+			array[i] = array[minIndex];
+			array[minIndex] = temp;
+		}
+
+		for (int i = 0; i<array.length ; i++){
+			System.out.println(array[i]);
+		}
+
+	}
+}
