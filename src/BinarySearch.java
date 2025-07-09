21671f45a714dca94694de0e9a2a198b88a7908c
kimjinhoo <wlsgnwkd22@naver.com>
2025-07-09 17:00:31 +0900

    MergeSort

---
+++ src/BinarySearch.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class BinarySearch {
+	// 이진 탐색
+	/*
+		start 포인터는 배열의 맨 앞(인덱스 0), end 포인터는 배열의 맨 뒤를 가리킵니다.
+
+		mid 포인터를 (start + end) / 2 위치에 둡니다.
+
+		mid 위치의 값과 찾으려는 목표 값(target)을 비교합니다.
+
+		arr[mid] == target 이면, 값을 찾았으므로 탐색을 종료합니다.
+
+		arr[mid] < target 이면, 목표 값이 더 오른쪽에 있다는 뜻이므로, 탐색 범위를 오른쪽 절반으로 좁힙니다. (start = mid + 1)
+
+		arr[mid] > target 이면, 목표 값이 더 왼쪽에 있다는 뜻이므로, 탐색 범위를 왼쪽 절반으로 좁힙니다. (end = mid - 1)
+
+		start 포인터가 end 포인터보다 커지면 탐색 범위에 더 이상 원소가 없다는 뜻이므로 탐색을 종료합니다.
+
+	*/
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt)
+			.sorted()
+			.toArray();
+		int M = Integer.parseInt(br.readLine());
+		int[] X = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt).toArray();
+
+
+		for(int i =0; i<X.length; i++){
+			int start = 0;
+			int end = A.length -1;
+			boolean found = false;
+
+
+			while(start <= end){
+				int mid = (start + end) / 2;
+
+				if(A[mid] > X[i]){
+					end  = mid-1;
+				}else if(A[mid] < X[i]){
+					start = mid+1;
+				}else{
+					found = true;
+					break;
+				}
+			}
+
+			if (found) {
+				System.out.println(1);
+			} else {
+				System.out.println(0);
+			}
+
+		}
+	}
+}
---
+++ src/MergeSort.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MergeSort {
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for(int i =0; i < N; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		merge_sort(arr , 0 , arr.length -1);
+
+		for (int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+
+
+	}
+
+
+	static void merge_sort(int[] arr , int start , int end){
+		if(start < end){
+			int mid = (start + end) / 2;
+			//왼쪽
+			merge_sort(arr,start,mid);
+			merge_sort(arr,mid + 1 , end);
+
+			merge(arr,start,mid,end);
+		}
+	}
+
+	static void merge(int[] arr,int start ,int mid ,int end){
+		int i = start , j =mid+1 , k=0;
+		int[] temp = new int[end - start + 1];
+
+		while(i <= mid && j <= end){
+			if(arr[i] <= arr[j]){
+				temp[k++] = arr[i++];
+			}else{
+				temp[k++] = arr[j++];
+			}
+		}
+
+		while(i <= mid){
+			temp[k++] = arr[i++];
+		}
+
+
+		while(j <= end){
+			temp[k++] = arr[j++];
+		}
+
+		for(int idx = 0; idx < k; idx++){
+			arr[start + idx] = temp[idx];
+		}
+
+	}
+}
